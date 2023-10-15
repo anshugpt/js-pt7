90497ef84616795b59b43faf3a13eaa4e3be746d
anshugpt <138896567+anshugpt@users.noreply.github.com>
2023-10-15 19:48:13 +0530

    update app.js
    added try and catch topic

--- app.js
+++ app.js
@@ -10,7 +10,28 @@ const student = {
     phy: 88,
     math: 78,
     getAvg(){
-        // return (eng + phy + math)/3 ; //* This will not work 
+        //// return (eng + phy + math)/3 ; //* This will not work 
         return (this.eng + this.phy + this.math)/3 ; //* This will work
     }
 }
+
+
+//* Try & Catch
+// The try statement allows you to define a block of code to be tested for errors while it is being executed
+// The catch statement allows you to define a block of code to be executed, if an error occurs in the try block
+// if we run big code and we got an error in the middle of the code after that error all the code did't run and websute crash
+// that why we test the code by try and catch
+
+// console.log("hello1");
+// console.log("hello1");
+// try {
+//     console.log(a);
+// } catch {
+//     console.log("get an error... a is not defined");
+// }
+// console.log("hello2");
+// console.log("hello2");
+
+// by this we can run all the code while getting error
+// we can see error in catch statement -> catch(err){ console.log(err);}
+// this will show which error we got in try!
